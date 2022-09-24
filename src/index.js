2a34980a0b3efc54d9f0c9ee80c70a7a819f13a8
dipeshkumarsah98 <dipeshsah98@gmail.com>
2022-09-24 09:25:13 +0545

    adding scroll to top in index page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./Components/ScrollToTop";
 import "./Styles/Style.css";
 import "flowbite/dist/flowbite.js";
 
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
